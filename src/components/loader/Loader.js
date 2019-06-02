241e72766e1ad5de2b076f337aefd6fb03c8c81d
Duy Vu <duyv.thanh@gmail.com>
2019-06-01 20:56:30 -0600

    Add loader component and background

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Loader = () => {
+  return (
+    <div>
+      <h1>Is Loading...</h1>
+      <img src={'https://media.giphy.com/media/zocX5Vxv9Bs9q/giphy.gif'} />
+    </div>
+  )
+}
+
+export default Loader;
