d50831747e43aec96bc4299628820badfde0526b
Duy Vu <duyv.thanh@gmail.com>
2019-06-02 16:13:04 -0600

    Add new components and actions to get category titles

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const isLoading = (boolean) => ({
   type: 'IS_LOADING',
   isLoading: boolean
 })
+
+export const getCategoryTitles = (categories) => ({
+  type: 'GET_CATEGORY_TITLES',
+  categories
+})
+
+export const getCurrentCategory = (categoryTitle) => ({
+  type: 'GET_CURRENT_CATEGORY',
+  categoryTitle
+})
