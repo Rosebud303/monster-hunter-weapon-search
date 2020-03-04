68e0375ffc0634293a58c343ac2845eff4e8f56d
Duy Vu <duyv.thanh@gmail.com>
2020-03-04 14:32:18 -0700

    Fix syntax errors in Weapon.js and Fixed routing issue in WeaponDetails.js

--- src/components/weapon/Weapon.js
+++ src/components/weapon/Weapon.js
@@ -6,13 +6,12 @@ export const Weapon = ({id, picture, name}) => {
   
   return (
     <Link id={id} className='weapon-card' to={`/weapons/${id}`}>
-      <p id={id} 
-        >
+      <p id={id}>
          {name}
       </p>
       <img id={id} className='weapon-image'
            src={picture}
-           />
+      />
     </Link>
   )
 }
